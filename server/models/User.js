9695e8e180d2c725427abd2eca4d446c699b500d
Nitishe27 <nitisheprem@gmail.com>
2024-11-16 06:07:07 -0800

    User-Initial-Change

--- server/models/User.js
+++ server/models/User.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },  // Ensuring unique emails
+    email: { type: String, required: true, unique: true },  
     password: { type: String, required: true },
-    score: { type: Number, default: 0 }  // Adding the score field with a default value of 0
+    score: { type: Number, default: 0 }  
 });
 
-// Creating and exporting the 'User' model
-const UserModel = mongoose.model('User', UserSchema);  // Changed 'Users' to 'User' to follow naming conventions
 
-module.exports = UserModel;  // Correctly exporting the User model
+const UserModel = mongoose.model('User', UserSchema);  
+
+module.exports = UserModel;  
